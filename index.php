f99be9560873ab7376f09b8455ea8fb6da26c950
Robert Orliński <r.s.orlinski@gmail.com>
2017-04-29 00:04:50 +0200

    Main file.

--- index.php
+++ index.php
@@ -11,19 +11,15 @@
 
 	<?php 
 		if (have_posts()) :
-			while (have_posts()) : the_post(); ?>
+		        while (have_posts()) : the_post(); ?>
 			<h2>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				<a href="<?php the_permalink(); ?>">
+	                                <?php the_title(); ?>
+	                        </a>
 			</h2>
-			<div class="post-info">
-				<div><?php the_category(); ?></div>
-				<div><?php the_author(); ?></div>
-				<div><?php the_date(); ?></div>
-				<div><?php the_tags(); ?></div>
-			</div>
-			
-			<?php the_content(); ?>
 
+			<?php the_content(); ?>
+				
 			<?php endwhile;
 
 			else :
