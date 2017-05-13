7d137eab32b4b6127418e906b0b48aa5ee8395ba
Robert Orliński <r.s.orlinski@gmail.com>
2017-05-13 14:40:59 +0200

    Plik główny

--- index.php
+++ index.php
@@ -1,31 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
-	<title>Modern Blog</title>
-	<?php wp_head(); ?>
-</head>
-<body>
+<?php 
+get_header();	
+	if (have_posts()) :
+		    while (have_posts()) : the_post(); ?>
+		<h2>
+			<a href="<?php the_permalink(); ?>">
+	        	<?php the_title(); ?>
+			</a>
+		</h2>
 
-	<h1>Modern Blog</h1>
-	<?php 
-		if (have_posts()) :
-		        while (have_posts()) : the_post(); ?>
-			<h2>
-				<a href="<?php the_permalink(); ?>">
-	            	<?php the_title(); ?>
-				</a>
-			</h2>
-
-			<?php the_content(); ?>
+		<?php the_content(); ?>
 				
-			<?php endwhile;
+		<?php endwhile;
 
-			else :
-				echo "<p>Na tej stronie nie ma żadnych wpisów.</p>";
-		endif;
-	?>	
-
-</body>
-</html>
+		else :
+			echo "<p>Na tej stronie nie ma żadnych wpisów.</p>";
+	endif;
+	
+get_footer();
+?>
