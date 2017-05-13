d56dde0df34bfcd4b5d3f1095d33e3347cbdc763
Robert Orliński <r.s.orlinski@gmail.com>
2017-05-13 14:40:42 +0200

    Funkcje motywu

--- functions.php
+++ functions.php
@@ -1,13 +1,22 @@
 <?php 
 	function loadingStyles(){
 	    wp_enqueue_style(
-	        'style',
-	        get_stylesheet_uri(),
+	        'bootstrap',
+	        get_template_directory_uri() . '/css/bootstrap.min.css',
 	        false,
 	        '1.1',
 	        'all'
 	    );
+	    wp_enqueue_style(
+	        'style',
+	        get_stylesheet_uri(),
+	        false,
+	        '1.0',
+	        'all'
+	    );
 	}
+	
+	
 
 	add_action('wp_enqueue_scripts', 'loadingStyles');
 ?>
