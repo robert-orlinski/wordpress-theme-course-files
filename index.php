4c15930b3b83d8f2418ac721551b9cb02dee647f
Robert Orliński <r.s.orlinski@gmail.com>
2017-07-24 13:33:17 +0200

    Plik główny

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
-<?php 
-get_header();	
-	if (have_posts()) :
-		while (have_posts()) : the_post(); ?>
-		
-		<article class="post">
-			<h2>
-				<a href="<?php the_permalink(); ?>">
-		        	<?php the_title(); ?>
-				</a>
-			</h2>
-			<?php the_content(); ?>
-		</article>
-				
-		<?php endwhile;
+<?php
+get_header(); ?>
 
-		else :
-			echo "<p>Na tej stronie nie ma żadnych wpisów.</p>";
-	endif;
-	
+	<main class="content">
+		<article class="blog-container">
+			<?php
+				if (have_posts()) :
+					while (have_posts()) : the_post(); ?>
+
+					<section class="post">
+						<h2 class="post-title">
+							<a class="post-link" href="<?php the_permalink(); ?>">
+					    	<?php the_title(); ?>
+							</a>
+						</h2>
+						<?php the_content(); ?>
+					</section>
+
+					<?php endwhile;
+
+					else :
+						echo "<p>Na tej stronie nie ma żadnych wpisów.</p>";
+				endif; ?>
+
+		</article>
+	</main>
+
+<?php
 get_footer();
 ?>
