36b58e71bbbb8440312eb8f98d1cf52a44ce0fff
Robert Orliński <r.s.orlinski@gmail.com>
2017-10-30 11:05:48 +0100

    Funkcje motywu

--- functions.php
+++ functions.php
@@ -14,16 +14,23 @@
 	        false,
 	        'all'
 	    );
-			wp_enqueue_style(
-	        'google-fonts',
-	        'https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&amp;subset=latin-ext'
-	    );
+			// wp_enqueue_style(
+	    //     'google-fonts',
+	    //     'https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&amp;subset=latin-ext'
+	    // );
 	}
-
 	add_action('wp_enqueue_scripts', 'loadingStyles');
 
-	register_nav_menus(array(
-        'primary' => 'Nawigacja główna',
-        'secondary' => 'Nawigacja w stopce'
-    ));
+	function themeFunctionality(){
+		register_nav_menus(array(
+	      'primary' => 'Nawigacja główna',
+	  		'secondary' => 'Nawigacja w stopce'
+	  ));
+
+		add_theme_support('post-thumbnails');
+		set_post_thumbnail_size(720, 370, true);
+
+	}
+	add_action('after_setup_theme', 'themeFunctionality');
+
 ?>
