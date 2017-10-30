f98bea45e35c48a07ca85d9de435b7d8cb9b26b4
Robert Orliński <r.s.orlinski@gmail.com>
2017-10-30 11:05:31 +0100

    Strona główna

--- index.php
+++ index.php
@@ -13,7 +13,30 @@ get_header(); ?>
 					    	<?php the_title(); ?>
 							</a>
 						</h2>
-						<?php the_content(); ?>
+						<p class="post-meta">
+								<?php
+									$categories = get_the_category();
+									$output = '';
+
+									if ($categories) {
+										foreach($categories as $category){
+											$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>, ';
+										}
+										echo trim($output, ', ');
+									}
+							 	?>
+							 	| Written by: <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> in
+							 	<?php the_time('F j, Y'); ?>
+					 		</p>
+						<div class="post-thumbnail">
+							<a href="<?php the_permalink(); ?>">
+								<?php the_post_thumbnail(); ?>
+							</a>
+						</div>
+						<div class="post-content">
+							<?php the_content(); ?>
+						</div>
+						<hr class="post-separator">
 					</section>
 
 					<?php endwhile;
